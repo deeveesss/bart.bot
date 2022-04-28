7406c671ca8a8444deb06a15ed07a378f721bb35
deeveesss <53666877+deeveesss@users.noreply.github.com>
2022-04-27 23:16:01 -0500

    fix spacing

--- cmds/av.js
+++ cmds/av.js
@@ -2,23 +2,23 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
+  data: new SlashCommandBuilder()
     .setName("av")
     .setDescription("get your or another user's avatar")
     .addUserOption(option => option
       .setName("target")
       .setDescription("specify a user")),
   run: async (interaction) => {
-		let arg = interaction.options;
+    let arg = interaction.options;
     let target = arg.getUser('target') ? arg.getUser('target') : interaction.user;
     let member = await interaction.guild.members.fetch(target.id);
     let avatar = await member.displayAvatarURL({dynamic: true, size: 1024});
     let richEmbed = new MessageEmbed()
       .setAuthor({ name: member.user.tag, iconURL: avatar })
-      .setImage(avatar);
+      .setImage(avatar)
+      .setColor("#bf8cff");
     await interaction.reply({ 
-      embeds: [richEmbed],
-      ephemeral: true
+      embeds: [richEmbed]
     });
-	},
+  },
 };
