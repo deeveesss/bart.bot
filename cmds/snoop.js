621751e18b72fa93affc42d4b1906bc5f189a75e
deeveesss <53666877+deeveesss@users.noreply.github.com>
2022-04-27 23:15:00 -0500

    fix spacing

--- cmds/snoop.js
+++ cmds/snoop.js
@@ -2,22 +2,22 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-	  .setName("snoop")
+  data: new SlashCommandBuilder()
+    .setName("snoop")
     .setDescription("catch the last deleted message"),
-	run: async (interaction, client) => {
+  run: async (interaction) => {
     let richEmbed = new MessageEmbed().setColor("RED").setAuthor({ name: "No snoops found." });
-    const snoop = client.snoops.get(`snoop-${interaction.channel.id}`);
-    if (!snoop) interaction.reply({ embeds: [richEmbed], ephemeral: true });
+    const snoop = await interaction.client.snoops.get(`snoop-${interaction.channel.id}`);
+    if (!snoop) await interaction.reply({ embeds: [richEmbed], ephemeral: true });
     else {
       richEmbed
         .setColor("GREEN")
         .setAuthor({ name: snoop.author, iconURL: snoop.avatar })
         .setDescription(snoop.content)
-        .setFooter({ text: "goteem" })
+        .setFooter({ text: "Snooped" })
         .setTimestamp(snoop.createdAt);
       if (snoop.attachments !== "") richEmbed.addField("Attachments",snoop.attachments);
-      interaction.reply({ embeds: [richEmbed] });
+      await interaction.reply({ embeds: [richEmbed] });
     }
-	},
+  },
 };
