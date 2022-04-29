8161ecd81894701ab536641c421344a255c8fc4a
deeveesss <53666877+deeveesss@users.noreply.github.com>
2022-04-28 20:23:46 -0500

    Add files via upload

---
+++ events/messageDelete.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'messageDelete',
+  run(message, client) {
+    if (message.author.bot) return;
+    let attached = "";
+    if (message.attachments.size >= 1) { message.attachments.forEach(file => { attached += `[Attachment](${file.url}\n)` }) }
+    client.snoops.set(`snoop-${message.channel.id}`, {
+      author: message.author.tag, avatar: message.author.displayAvatarURL(),
+      content: message.content, attachments: attached,
+      createdAt: message.createdTimestamp,
+    });
+  },
+};
---
+++ events/messageUpdate.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'messageUpdate',
+  run(oldMessage, newMessage, client) {
+    if (newMessage.author.bot) return;
+    if (oldMessage.content.toString() === newMessage.content.toString()) return;
+    client.snoops.set(`edit-${newMessage.channel.id}`, {
+      author: newMessage.author.tag, avatar: newMessage.author.displayAvatarURL(),
+      newcontent: newMessage.content, oldcontent: oldMessage.content,
+      createdAt: newMessage.createdTimestamp,
+    });
+  },
+};
