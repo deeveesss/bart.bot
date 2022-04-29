26352ba5fa38286d120ffafabed80d300cfb99a2
deeveesss <53666877+deeveesss@users.noreply.github.com>
2022-04-28 20:20:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 // initialize bot client instance
-const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, Collection, Intents, MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const { token, prefix, devId } = require('./config.json');
 const client = new Client({ intents: [
   Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS,
   Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, ],
 });
-//load commands from files
-const fs = require('node:fs');
-client.commands = new Collection();
-const cmdFiles = fs.readdirSync('./cmds').filter(file => file.endsWith('.js'));
-for (const file of cmdFiles) { const cmd = require(`./cmds/${file}`); client.commands.set(cmd.data.name, cmd) }
-//console update
-client.once('ready', () => { console.log(`[Status] Online.`); client.user.setActivity("you.", {type:"WATCHING"}) });
+
 //snooping
-client.snoops = new Collection();
-client.on("messageDelete", async message => {
-  if (message.author.bot) return;
-  let attached = "";
-  if(message.attachments.size >= 1) { message.attachments.forEach(file => { attached += `[Attachment](${file.url}\n)` }) }
-  client.snoops.set(`snoop-${message.channel.id}`, {
-    author: message.author.tag, avatar: message.author.displayAvatarURL(),
-    content: message.content, attachments: attached,
-    createdAt: message.createdTimestamp,
-  });
-});
-client.on("messageUpdate", async (oldMessage, newMessage) => {
-  if (newMessage.author.bot) return;
-  if(oldMessage.content.toString()===newMessage.content.toString()) return;
-  client.snoops.set(`edit-${newMessage.channel.id}`, {
-    author: newMessage.author.tag, avatar: newMessage.author.displayAvatarURL(),
-    newcontent: newMessage.content, oldcontent: oldMessage.content,
-    createdAt: newMessage.createdTimestamp,
-  });
-});
-//processing
 client.games = new Collection();
+client.snoops = new Collection();
+client.buttons = new Collection();
+client.commands = new Collection();
+
+//load from files
+const fs = require('node:fs');
+
+const cmdFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of cmdFiles) { const command = require(`./commands/${file}`); client.commands.set(command.data.name, command) }
+
+const btnFiles = fs.readdirSync('./buttons').filter(file => file.endsWith('.js'));
+for (const file of btnFiles) { const button = require(`./buttons/${file}`); client.buttons.set(button.name, button) }
+
+const evtFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of evtFiles) {
+  const e = require(`./events/${file}`);
+  if (e.once) client.once(e.name, (...args) => e.run(...args));
+  else client.on(e.name, (...args) => e.run(...args, client));
+}
+
+//processing
 client.on('interactionCreate', async interaction => {
-  if (interaction.isButton()) interaction.deferUpdate();
-  if (interaction.isCommand()) {  
-  const command = client.commands.get(interaction.commandName);
-  if (!command) return;
-  try { await command.run(interaction, client); } 
-  catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  if (interaction.isCommand()) {
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+    try { await command.run(interaction, client); } 
+    catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
+  }
+  if (interaction.isButton()) {
+    const button = client.buttons.get(interaction.customId);
+    if (!button) return;
+    try { await button.run(interaction, client); } 
+    catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this button!', ephemeral: true });
+    } 
   }
 });
 
 //login
 client.login(token);
+
 //process errors
 process.on('unhandledRejection', o_O => console.error(`Uncaught Promise Error:\n${o_O.stack}`));
