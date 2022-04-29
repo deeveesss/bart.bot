fe2cf0553fd4297e9515541f680cdf2f16a34369
deeveesss <53666877+deeveesss@users.noreply.github.com>
2022-04-28 20:28:52 -0500

    Update register.js

--- register.js
+++ register.js
@@ -3,9 +3,10 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, serverId, token } = require('./config.json');
 
+// export for command later
 const commands = [];
-const cmdFiles = fs.readdirSync('./cmds').filter(file => file.endsWith('.js'));
-for (const file of cmdFiles) { const cmd = require(`./cmds/${file}`); commands.push(cmd.data.toJSON()) }
+const cmdFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of cmdFiles) { const cmd = require(`./commands/${file}`); commands.push(cmd.data.toJSON()) }
 
 const rest = new REST({ version: '9' }).setToken(token);
 rest.put(Routes.applicationGuildCommands(clientId, serverId), { body: commands })
