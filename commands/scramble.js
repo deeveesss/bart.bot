d69a23f11247e1133f4e7064d595dd5f2a743ea8
dvs <53666877+deeveesss@users.noreply.github.com>
2022-05-01 20:00:41 -0500

    Update scramble.js

--- commands/scramble.js
+++ commands/scramble.js
@@ -9,15 +9,40 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("scramble")
     .setDescription("unscramble words before your opponents"),
-  
+  //-- difficulty filters --
+    .addIntegerOption(option => 
+      option
+        .setName("difficulty")
+        .setDescription("The the difficulty of the game")
+        .setMinValue(1)
+        .setMaxValue(3) ),
   run: async (interaction) => {
-    
-    let scrambleDictionary = [].concat(words),
-      word = shuffle(scrambleDictionary)[0],
+    let scrambleDictionary = [];
+//-- difficulty filters --
+    let difficulty = interaction.options.getInteger("difficulty");
+
+    switch (difficulty) {
+      default:
+        for (const e of words) if (e.word.length <= 5) scrambleDictionary.push(e);
+        break;
+      case 1:
+        for (const e of words) if (e.word.length >= 4 && e.word.length <= 6) scrambleDictionary.push(e);
+        break;
+      case 2:
+        for (const e of words) if (e.word.length >= 5 && e.word.length <= 8) scrambleDictionary.push(e); 
+        break;
+      case 3:
+        for (const e of words) if (e.word.length >= 7) scrambleDictionary.push(e); 
+        break;
+    }
+// shuffle the list and grab first object,
+// split the word into an array, shuffle the array,
+// join the array back together, set winner variable.    
+    let word = shuffle(scrambleDictionary)[0],
       scrambling = shuffle(word.split("")), 
       scrambledWord = scrambling.join(" "),
       winner = null;
-
+    
     let richEmbed = new MessageEmbed()
       .setAuthor({ name: `Scramble Time!` })
       .setDescription(`${interaction.user} started scramble!\nThe word to unscramble is:\n` + scrambledWord)
